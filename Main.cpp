ff7e7bb9a0d826c71f038f62588bb83935d5bdba
Tavid Terzian <terzian@chapman.edu>
2020-11-16 20:18:06 -0800

    Submission

--- Main.cpp
+++ Main.cpp
@@ -2,12 +2,16 @@
 #include "Input.h"
 #include "Student.h"
 #include "Queue.h"
+#include "Room.h"
 using namespace std;
 
-int main() {
-	Input *read = new Input("test.txt");
-	read->printList();
+int main(int argc, char** argv) {
+	Input *read = new Input(argv[1]);
+	Queue<Student> line = read->getLine();
+	Room main(line, read->getStdNum(), read->getWindowNum());
+	main.startDay();
 
+	delete read;
 	system("pause");
 	return 0;
 }
